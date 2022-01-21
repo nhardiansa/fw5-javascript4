ab6e3af2c37848fd8ed85231051f5c5ffadf6bbc
Nabil Hardiansa <nhardiansa@gmail.com>
2022-01-21 14:37:46 +0800

    menginstall enquirer dan memodifikasi tugas 1

---
+++ 1.js
@@ -0,0 +1,60 @@
+const { prompt } = require('enquirer');
+
+const palindromDetector = async () => {
+  let answer;
+
+  do {
+    answer = await prompt({
+      type: 'input',
+      name: 'text',
+      message: 'Masukan text'
+    })
+  } while (!validator(answer.text))
+
+  const { text } = answer;
+
+  const result = text.split('').reverse().join('')
+
+  if (text === result) {
+    console.log('palindrom');
+  } else {
+    console.log('bukan palindrom');
+  }
+}
+
+/*=============== validator input =============== */
+const validator = (input) => {
+  if (input === '') {
+    console.log('Input tidak boleh kosong');
+    return false;
+  }
+
+  if (input.length < 2) {
+    console.log('Teks terlalu pendek');
+    return false;
+  }
+
+  return true
+}
+
+// const askQuestion = async (type, question) => {
+//   try {
+//     let answer
+//     do {
+//       answer = await prompt({
+//         type: type,
+//         name: 'text',
+//         message: question
+//       })
+
+//     } while (!validator(answer.text))
+
+//     console.log(answer);
+
+//     return answer;
+//   } catch (err) {
+    
+//   }
+// }
+
+palindromDetector()
