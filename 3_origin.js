22b93167fa122d636e32acd11f2df6f25a202a36
Nabil Hardiansa <nhardiansa@gmail.com>
2022-01-24 06:47:30 +0800

    membuat tugas 3 tanpa builtin function

--- 3_origin.js
+++ 3_origin.js
@@ -1,60 +1,81 @@
-const number = 5956560159466056;
-let text = number.toString();
-const length = text.length - 1;
-let penampung = "";
-let hasil = []
+const split = (str, key) => {
+  const length = str.length
+  let dump = "";
+  const result = []
 
-
-for(let i = 0; i <= length; i++) {
-  if (i === length) {
-    penampung += text[i]
-    let idx = hasil.length
-    hasil[idx] = penampung
-  } else if (text[i] !== '0') {
-    penampung += text[i]
-  } else {
-    let idx = hasil.length
-    hasil[idx] = penampung
-    penampung = ""
+  for(let i = 0; i <= length; i++) {
+    if (key === "") {
+      if (str[i]) {
+        let nextIdx = result.length
+        result[nextIdx] = str[i]
+      }
+    } else if (i === length || str[i] === key) {
+      let nextIdx = result.length
+      result[nextIdx] = dump
+      dump = ""
+    } else {
+      dump += str[i]
+    }
   }
+
+  return result;
 }
 
-const comparator = (val1, val2) => {
-  let res = Number(val1) - Number(val2)
+const join = (arr, separator=null) => {
+  const length = arr.length
+  let dump = ""
 
-  if (res === 0) {
-    return null
-  } else if (res < 0) {
-    return false
-  } else {
-    return true
+  for(let i = 0; i < length; i++) {
+    if (i === 0) {
+      dump = arr[i]
+    } else if (separator == null) {
+      dump += "," + arr[i]
+    } else {
+      dump += separator + arr[i]
+    }
   }
+
+  return dump
 }
 
-for(let i = 0; i < hasil.length; i++) {
-  let el = hasil[i]
-  // for(let j = 0; j < el.length; j++) {
-  //   let a = el[j]
-  //   let b = el[j+1]
-  //   let res = a - b
+const sorter = (array) => {
+  const arr = array
+  const n = arr.length
+  console.log(arr, 'sort');
+  
+  for (let i = 1; i < n; i++)
+  { 
+    let key = arr[i]; //11
+    let j = i - 1; //0
 
-  //   if(res > 0) {
-  //     el[j] = b
-  //     el[j+1] = a
-  //   }
-    
-  //   if (res < 0) {
-  //     el[j] = a
-  //     el[j+1] = b
-  //   }
-  // }
-  el = el.split('').sort().join('')
-  // console.log(el);
-  hasil[i] = el
+    while (j >= 0 && arr[j] > key)
+    {
+      arr[j + 1] = arr[j]; 
+      j--; 
+    }
+    arr[j + 1] = key; 
+  }
+  
+  return arr
 }
 
-console.log(Number(hasil.join('')));
-// let dump = hasil[0]
-// hasil[0] = hasil[1]
-// hasil[1] = dump
-// console.log(hasil);
+const divideAndSort = (num, separator) => {
+  const text = String(num)
+  const key = String(separator) || ""
+  const arr = split(text, key)
+  // const arr = split("text", "")
+  let result = ""
+
+  for(let i = 0; i < arr.length; i++) {
+    let str = split(arr[i], "")
+    str = sorter(str)
+    result += join(str, "")
+  }
+
+  console.log(Number(result));
+}
+
+divideAndSort(5956560159466056, 0)
+// const number = 5956560159466056;
+// console.log(split('5956560159466056', '0'));
+// console.log(split('text', ''));
