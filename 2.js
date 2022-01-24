401191eeb5e649f44384fc5ace7733d39632c781
Nabil Hardiansa <nhardiansa@gmail.com>
2022-01-24 18:27:47 +0800

    menambahkan keterangan alur dengan komentar

--- 2.js
+++ 2.js
@@ -1,24 +1,26 @@
-const { prompt } = require('enquirer');
+const { prompt } = require('enquirer'); // melakukan inisialisasi variabel untuk package enquirer
 
-const reverseSentence = async () => {
+const reverseSentence = async () => { // inisialisasi fungsi ini untuk meminta jawaban dari user berdasarkan pertanyaan yang dibuat
   let answer;
 
+  // diawali dengan perulangan do while yang akan melakukan pengulangan pertanyaan jika jawaban yang diminta tidak sesuai dengan aturan validasi yang telah dibuat
   do {
-    answer = await prompt({
+    answer = await prompt({ 
       type: 'input',
       name: 'text',
       message: 'Masukan text'
     })
-  } while (!validator(answer.text.trim()))
+  } while (!validator(answer.text.trim())) // pengecekan kondisi dengan memanggil fungsi validator yang menerima jawaban dari user untuk dilakukan validasi
 
-  const { text } = answer;
+  const { text } = answer; // jika jawaban tervalidasi maka jawaban disimpan ke dalam variabel text
 
-  const result = mainProcess(text.trim())
+  const result = mainProcess(text.trim()) // dan jawaban dikirim sebagai parameter ke fungsi mainProcess agar bisa diolah sesuai keperlua dan tidak lupa dipanggil juga fungsi trim() untuk menghindari spasi yang tidak diperlukan serta hasil olahan tersebut akan disimpan ke dalam variabel result
 
-  console.log(result);
+  console.log(result); // isi dari variabel result akan ditampilkan
 }
 
 /*=============== validator =============== */
+// dalam fungsi validator ini akan menerima parameter input dalam bentuk string dan akan dicek sesuai dengan validasi yg ingin dibuat serta jika tidak tervalidasi akan mengembalikan nilai boolean false dan jika tervalidasi maka akan mengembalikan nilai boolean true
 const validator = (input) => {
   if (input === '') {
     console.log('Input tidak boleh kosong');
@@ -39,6 +41,7 @@ const validator = (input) => {
 }
 
 /*=============== pemrosesan kalimat =============== */
+//  dalam fungsi ini, paramater yang diterima adalah sebuah string yang akan diproses dengan memisah dengan spasi lalu dibalik menggunakan fungsi reverse dan digabung kembali dengan fungsi join serta mengembalikan hasil dari proses tersebut
 const mainProcess = (str) => {
   let sentence = str
 
@@ -49,4 +52,4 @@ const mainProcess = (str) => {
   return sentence
 }
 
-reverseSentence()
+reverseSentence() // fungsi reverseSentence dipanggil untuk dijalankan
